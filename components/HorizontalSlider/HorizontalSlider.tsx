d437d617458a38d0a979a941b083f586207425a0
Sergio Fruto <fruto.sergio@gmail.com>
2023-03-08 15:18:01 -0300

    horizontal slider now working with framer motion(wip)

--- components/HorizontalSlider/HorizontalSlider.tsx
+++ components/HorizontalSlider/HorizontalSlider.tsx
@@ -1,58 +1,74 @@
 import { useState, useRef, useEffect} from 'react';
 import {SliderContainer, Slider, Slide} from './HorizontalSlider.styled';
+import { motion } from "framer-motion";
+import styles from './HorizontalSlider.module.css';
 
 const HorizontalSlider = () => {
   const mediaArr = [
-    {name: "placeholder", img: "https://plus.unsplash.com/premium_photo-1664005044945-ab3a1282ac01?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"},
-    {name: "placeholder", img: "https://images.unsplash.com/photo-1668018064529-76c9849d6e0e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80"},
-    {name: "placeholder", img: "https://images.unsplash.com/photo-1669630814501-9b0dd220a763?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80"},
-    {name: "placeholder", img: "https://images.unsplash.com/photo-1672487914895-73be33e58283?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"},
-    {name: "placeholder", img: "https://images.unsplash.com/photo-1672818640506-d5fb6372ed40?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80"},
-    {name: "placeholder", img: "https://images.unsplash.com/photo-1671627185063-9a7ec403574d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2032&q=80"},
-    {name: "placeholder", img: "https://plus.unsplash.com/premium_photo-1672088104722-5ac457f1d289?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80"},
-    {name: "placeholder", img: "https://images.unsplash.com/photo-1672080070747-9b28b4b403af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80"},
-    {name: "placeholder", img: "https://images.unsplash.com/photo-1673031288723-f198cd527b97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"},
-    {name: "placeholder", img: "https://images.unsplash.com/photo-1671347020855-8f35d210ee9a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2664&q=80"}
+    {
+      name: "placeholder",
+      img: "https://images.unsplash.com/photo-1678263093715-ba00dc80520f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80",
+    },
+    {
+      name: "placeholder",
+      img: "https://images.unsplash.com/photo-1668018064529-76c9849d6e0e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80",
+    },
+    {
+      name: "placeholder",
+      img: "https://images.unsplash.com/photo-1669630814501-9b0dd220a763?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80",
+    },
+    {
+      name: "placeholder",
+      img: "https://images.unsplash.com/photo-1672487914895-73be33e58283?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+    },
+    {
+      name: "placeholder",
+      img: "https://images.unsplash.com/photo-1672818640506-d5fb6372ed40?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80",
+    },
+    {
+      name: "placeholder",
+      img: "https://images.unsplash.com/photo-1671627185063-9a7ec403574d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2032&q=80",
+    },
+    {
+      name: "placeholder",
+      img: "https://plus.unsplash.com/premium_photo-1672088104722-5ac457f1d289?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80",
+    },
+    {
+      name: "placeholder",
+      img: "https://images.unsplash.com/photo-1672080070747-9b28b4b403af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80",
+    },
+    {
+      name: "placeholder",
+      img: "https://images.unsplash.com/photo-1673031288723-f198cd527b97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
+    },
+    {
+      name: "placeholder",
+      img: "https://images.unsplash.com/photo-1671347020855-8f35d210ee9a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2664&q=80",
+    },
   ];
 
+  const [sliderWidth, setSliderWidth] = useState(0);
   const sliderRef = useRef();
-  const [xValue, setXValue] = useState('');
-  const [yValue, setYValue] = useState('');
 
-  const getPosition = (e) => {
-    const x = e.clientX;
-    setXValue(x);
-
-    const y = e.clientY;
-    setYValue(y);
-  };
-
-  const handleOnDown = (e) => {
-    getPosition(e);
-    console.log(xValue);
-    console.log(yValue);
-  };
-
-  // const handleMouseMove = (e) => {
-  //   const mouseDelta = parseFloat(xValue) - e.clientX;
-  //   const maxDelta = window.innerWidth / 2;
-  //   const percentage = (mouseDelta / maxDelta) * -100;
-  //   const nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage;
-  //   const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-  // }
-
-  // useEffect(() => {
-  //   getPosition();
-  // }, []);
+  useEffect(() => {
+    setSliderWidth(sliderRef.current.scrollWidth - sliderRef.current.offsetWidth);
+  }, []);
 
   return (
-    <SliderContainer onClick={handleOnDown}>
-      <Slider ref={sliderRef}>
-        { mediaArr.map((media) => (
-          <Slide src={media.img} alt={media.name} key={media.img}/>
+    <motion.div className={styles["slider"]} ref={sliderRef}>
+      <motion.div
+        drag="x"
+        dragConstraints={{ right: 0, left: -sliderWidth }}
+        whileTap={{cursor: "grabbing"}}
+        className={styles["slider-inner"]}
+      >
+        {mediaArr.map((media) => (
+          <motion.div className={styles["slider-item"]}>
+            <img src={media.img} alt={media.name} key={media.img} />
+          </motion.div>
         ))}
-      </Slider>
-    </SliderContainer>
+      </motion.div>
+    </motion.div>
   );
 }
  
